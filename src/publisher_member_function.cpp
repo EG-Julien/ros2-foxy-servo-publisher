f38fce0cc6cb5c28ecb8aff81fd5ed1f760f04d7
Julien Geneste <julien@aircslab.fr>
2023-05-03 16:12:59 +0200

    servo angle pub

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -27,9 +27,9 @@ class MinimalPublisher : public rclcpp::Node
 {
 public:
   MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+  : Node("ros_servo_publisher"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    publisher_ = this->create_publisher<std_msgs::msg::String>("servo_ctrl", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
@@ -38,10 +38,16 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
+
+    if (count_ > 360) {
+      count_ = 0;
+    }
+    count_++;
+    message.data = "servo_1:" + std::to_string(count_) + ", servo_2:" + std::to_string(360 - count_);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
